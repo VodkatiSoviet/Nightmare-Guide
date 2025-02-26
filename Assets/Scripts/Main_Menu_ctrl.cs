79b1bc51e03de93abde021d07cf6144539d95c69
KJI <145641961+VodkatiSoviet@users.noreply.github.com>
2025-02-27 04:17:13 +0900

    메인메뉴 UI
    목업, 기본 기능 추가

---
+++ Assets/Scripts/Main_Menu_ctrl.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Main_Menu_ctrl : MonoBehaviour
+{
+    [Header("Alert_Page")]
+    public GameObject blocker;
+    public GameObject Alert_Page;
+    public Text Alert_Msg;
+    public Button yes_Btn;
+    public Button no_Btn;
+
+    private void Start()
+    {
+        Debug.Log("초기화");
+        blocker.SetActive(false);
+        Alert_Page.SetActive(false);
+    }
+    private void Update()
+    {
+        if(Input.GetKey(KeyCode.A))
+        {
+            string msg = "경고창 테스트";
+            Show_Alert_Page(msg);
+        }
+    }
+    //마우스에 무슨 메소드 쓴거야?
+    //버튼이 안눌리잖아
+
+    //메인 메뉴 메소드
+    public void Click_Start_Btn()
+    {
+        //SceneManager.LoadScene("");
+        Debug.Log("게임 시작");
+    }
+
+    public void Click_Load_Btn()
+    {
+        Debug.Log("세이브 파일 불러오기");
+        Show_Alert_Page("읎어요");
+    }
+
+    public void Click_Setting_Btn()
+    {
+        Debug.Log("설정");
+        Show_Alert_Page("읎어요");
+    }
+
+    public void Click_Exit_Btn()
+    {
+        Debug.Log("게임 종료");
+        Application.Quit();
+    }
+
+    //경고창 페이지
+
+    public void Show_Alert_Page(string msg)
+    {
+        blocker.SetActive(true);
+        Alert_Page.SetActive(true);
+
+        Alert_Msg.text = msg;
+    }
+
+    public void Close_Alert_Page()
+    {
+        blocker.SetActive(false);
+        Alert_Page.SetActive(false);
+    }
+
+    public void Click_Alert_Yes_Btn()
+    {
+        Debug.Log("click Yes");
+        Close_Alert_Page();
+    }
+
+    public void Click_Alert_No_Btn()
+    {
+        Debug.Log("click No");
+        Close_Alert_Page();
+    }
+}
